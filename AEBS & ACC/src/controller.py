88c56094eaaf2cd61f93df942f5d2fdd47839e5d
weiber23727698 <b09902123@ntu.edu.tw>
2022-11-17 01:05:19 +0800

    update

---
+++ AEBS & ACC/src/controller.py
@@ -0,0 +1,55 @@
+from carla import Vector3D, CityObjectLabel
+import carla
+
+
+events = None
+
+def step(my_car):
+    ## TODO
+    ## Apply control to the car. See the API here.
+    ## https://carla.readthedocs.io/en/latest/python_api/#carla.VehicleControl
+    control = my_car.get_control()
+    vel = my_car.get_velocity().length() # m/s
+    d = on_sensor_data(events, my_car)
+    if(d < 10.75):
+        control.throttle = 0.0
+        control.brake = 1/(20-vel*3.6)
+        my_car.apply_control(control)
+       # print("stop here ")
+        return
+    ttc = 100
+    if(vel > 0):
+        ttc = d / vel
+    if(ttc<3.6):
+        control.throttle = 0.4
+        control.brake = 1.0
+    elif(vel*3.6 < 20):
+        control.throttle = 1.0
+        control.brake = 0.0
+    elif(vel*3.6>=20 and vel*3.6<=22):
+        control.throttle = (22-vel*3.6)*0.5
+        control.brake = 0.0
+    elif(vel*3.6 > 22):
+        control.throttle = 0.0
+        control.brake = 1.0
+    my_car.apply_control(control)
+
+
+
+def on_sensor_data(event, my_car):
+    ## Find points casted on vehicles
+    vehicle_tag = int(CityObjectLabel.Vehicles) # coach car
+    vehicle_points = filter(lambda det: det.object_tag == vehicle_tag, event) # get the lidar on the coach car
+    
+    ## TODO
+    ## Find the distance to the coach car using LiDAR points. You may check this.
+    ## https://carla.readthedocs.io/en/latest/python_api/#carla.SemanticLidarDetection
+    global events
+    events = event
+
+
+    coach = 0.0
+    for data in vehicle_points:
+        d =  data.point.distance(carla.Location(0, 0, 0))
+        coach = max(coach, d)
+    return coach
---
+++ Waypoint tracking/src/controller.py
@@ -0,0 +1,76 @@
+from enum import Enum
+from pickle import FALSE
+from carla import Vector3D, CityObjectLabel
+import carla
+import csv
+from math import *
+
+WAYPOINTS = [] # x decrease, y increase (steer<0 y變大)
+target = 0 # index of waypoint
+stage = 1
+
+def load_waypoints(path):
+    global WAYPOINTS
+    with open(path) as f:
+        rows = csv.reader(f)
+        WAYPOINTS = [[float(x) for x in row] for row in rows]
+
+def step(my_car):
+    
+    global WAYPOINTS   # The waypoints are stored here
+    global target 
+    global stage
+
+    car_orientation = my_car.get_transform().rotation.yaw   # Car orientation
+    loc = (my_car.get_location().x, my_car.get_location().y)   # Car location
+    print(loc)
+    if(loc[1] > WAYPOINTS[24][1]):
+        stage = 4
+    
+    if(target == 25):
+        stage = 4
+
+    ## TODO
+    ## Apply control to the car. See the API here.
+    ## https://carla.readthedocs.io/en/latest/python_api/#carla.VehicleControl
+    
+    control = my_car.get_control()
+    if(stage == 4):
+        control.throttle = 0.0
+        control.brake = 1.0
+    else:
+        control.throttle = 0.2
+        control.brake = 0.0
+        
+        dx = WAYPOINTS[target][0] - loc[0]
+        dy = WAYPOINTS[target][1] - loc[1]
+        tan1 = dy/dx # 應該前進的方向
+        tan2 = tan(car_orientation/180 * pi) # 車的方向
+        diff = (tan1-tan2)/(1+tan1*tan2) # tan(a-b)
+        print("%f %f %f" %(tan1, tan2, diff))
+        if(abs(diff) <= 0.2):
+            control.steer = 0
+        elif(abs(diff) <= 0.4):
+            control.steer = 0.1
+        elif(abs(diff) <= 0.6):
+            control.steer = 0.14
+        elif(abs(diff) <= 0.8):
+            control.steer = 0.18
+        elif(abs(diff) <= 1.0):
+            control.steer = 0.22
+        elif(abs(diff) <= 1.4):
+            control.steer = 0.26
+        elif(abs(diff) <= 1.8):
+            control.steer = 0.30
+        elif(abs(diff) <= 2.3):
+            control.steer = 0.35
+        else:
+            control.steer = 0.4
+
+
+        if(diff > 0):
+            control.steer *= -1
+
+
+    print("steer: %f" %(control.steer))
+    my_car.apply_control(control)
